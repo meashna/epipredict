06bb757a26e78ea3288eb44fac595d4cef8c8119
meashna <ashnarys1320@gmail.com>
2024-10-06 22:12:42 +0530

    add vercel

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const patientRoutes = require('./routes/patientRoutes');
 const consultationRoutes = require('./routes/consultationRoutes');
+const connectDB = require('./config/db');
+require('dotenv').config();  // Load environment variables from .env file
+
 const app = express();
 
-app.use(express.json());  // Parse JSON request bodies
+// Connect to the database
+connectDB();
+
+// Middleware to parse JSON request bodies
+app.use(express.json());  
+
+// Root route
 app.get('/', (req, res) => {
-    res.send('Server is running!');
-  });
+  res.send('Server is running!');
+});
+
+// Routes
 app.use('/', authRoutes);  // Use the auth routes for handling requests
 app.use('/patients', patientRoutes);
 app.use('/consultations', consultationRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
